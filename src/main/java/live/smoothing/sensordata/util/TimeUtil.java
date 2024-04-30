c63ee2f45528c0b004a1355c6de11da3f22204b2
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-04-30 22:38:38 +0900

    [Docs]: TimeUtil of JavaDoc

--- src/main/java/live/smoothing/sensordata/util/TimeUtil.java
+++ src/main/java/live/smoothing/sensordata/util/TimeUtil.java
@@ -4,10 +4,22 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * 시간 관련 유틸리티 클래스
+ *
+ * @author 신민석
+ */
 public class TimeUtil {
 
     private TimeUtil() {}
 
+    /**
+     * 최근 시간을 반환한다.
+     *
+     * @param source 기준 시간
+     * @param offset 시간 단위
+     * @return 최근 시간
+     */
     public static Instant getRecentHour(Instant source, long offset) {
         int hourOfDay = source.atZone(ZoneId.systemDefault()).getHour();
         long truncatedHour = hourOfDay / offset * offset;
@@ -17,6 +29,13 @@ public class TimeUtil {
                 .minus(9L, ChronoUnit.HOURS);
     }
 
+    /**
+     * 최근 분을 반환한다.
+     *
+     * @param source 기준 시간
+     * @param offset 분 단위
+     * @return 최근 분
+     */
     public static Instant getRecentMinute(Instant source, long offset) {
         int minuteOfHour = source.atZone(ZoneId.systemDefault()).getMinute();
         long truncatedMinute = minuteOfHour / offset * offset;
