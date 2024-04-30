d9bfdcb6c7eac90d4090f7780bc8299f426db9eb
nakta-y <nakta.y.biz@gmail.com>
2024-04-30 15:09:05 +0900

    [Feat] : 10분, 1시간 기준 전력 조회 구현

---
+++ src/main/java/live/smoothing/sensordata/repository/WattRepository.java
@@ -0,0 +1,12 @@
+package live.smoothing.sensordata.repository;
+
+import live.smoothing.sensordata.dto.Watt;
+
+import java.time.Instant;
+import java.util.List;
+
+public interface WattRepository {
+
+    List<Watt> getRawWattData(Instant start, String[] topic, String measurement);
+    List<Watt> getAggregateWattData(Instant start, String[] topic, String measurement);
+}
