fc23f2f57e2979aec100756cdfe45b07c07ad4ab
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-04-30 22:30:46 +0900

    [Docs]: KwhRepositoryImpl of JavaDoc

--- src/main/java/live/smoothing/sensordata/repository/impl/KwhRepositoryImpl.java
+++ src/main/java/live/smoothing/sensordata/repository/impl/KwhRepositoryImpl.java
@@ -15,6 +15,11 @@ import java.util.List;
 
 import static live.smoothing.sensordata.util.FluxUtil.getFlux;
 
+/**
+ * InfluxDB를 이용한 KwhRepository 구현체
+ *
+ * @author 신민석
+ */
 @Service
 @RequiredArgsConstructor
 public class KwhRepositoryImpl implements KwhRepository {
@@ -24,6 +29,12 @@ public class KwhRepositoryImpl implements KwhRepository {
     private static final String AGGREGATION2_BUCKET = "aggregation2";
     private static final String RAW_BUCKET = "powermetrics_data";
 
+    /**
+     * InfluxDB에 저장된 데이터를 조회하여 24시간 동안의 데이터를 1시간 단위로 반환(집계된 데이터)
+     *
+     * @param topics 조회할 topic의 이름들
+     * @return 집계된 Kwh의 리스트를 반환
+     */
     @Override
     public List<Kwh> get24HourData(String[] topics) {
         Flux query =
@@ -39,6 +50,12 @@ public class KwhRepositoryImpl implements KwhRepository {
         }
     }
 
+    /**
+     * InfluxDB를 조회하여 7일 동안의 데이터를 1일 단위로 반환(집계 데이터)
+     *
+     * @param topics 조회할 topic의 이름들
+     * @return 집계된 Kwh의 리스트를 반환
+     */
     @Override
     public List<Kwh> getWeekData(String[] topics) {
         Flux query =
@@ -55,6 +72,12 @@ public class KwhRepositoryImpl implements KwhRepository {
         }
     }
 
+    /**
+     * InfluxDB에 저장된 데이터를 조회하여 24시간 동안의 데이터를 반환(raw 데이터)
+     *
+     * @param topics 조회할 topic의 이름들
+     * @return 원시 Kwh의 리스트를 반환
+     */
     @Override
     public List<Kwh> get24Raw(String[] topics) {
 
@@ -71,6 +94,12 @@ public class KwhRepositoryImpl implements KwhRepository {
         }
     }
 
+    /**
+     * InfluxDB에 저장된 데이터를 조회하여 7일 동안의 데이터를 반환(raw 데이터)
+     *
+     * @param topics 조회할 topic의 이름들
+     * @return 원시 Kwh의 리스트를 반환
+     */
     @Override
     public List<Kwh> getWeekRaw(String[] topics) {
 
