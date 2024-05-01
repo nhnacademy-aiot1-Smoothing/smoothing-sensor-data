5d7a96a9edd459da507f6f9d5800b727fef98fea
supaicy <143978573+supaicy@users.noreply.github.com>
2024-05-01 14:06:51 +0900

    [Feat]: dto 구현
    API 요청후 응답을 받을때 사용.

--- src/main/java/live/smoothing/sensordata/dto/usage/EnergyUsageResponse.java
+++ src/main/java/live/smoothing/sensordata/dto/usage/EnergyUsageResponse.java
@@ -1,2 +1,15 @@
-package live.smoothing.sensordata.dto.usage;public class EnergyUsageResponse {
+package live.smoothing.sensordata.dto.usage;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class EnergyUsageResponse {
+
+    @JsonProperty("data")
+    List<EnergyUsage> data;
 }
