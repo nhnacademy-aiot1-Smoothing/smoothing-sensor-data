7e6bc49ce6d43f99e37a53c17f8750fc3c6ab2b4
supaicy <143978573+supaicy@users.noreply.github.com>
2024-05-01 14:07:27 +0900

    [Feat]: 임시 Config 파일
    - 추후에 Config 파일로 변경예정

--- src/main/java/live/smoothing/sensordata/adapter/Url.java
+++ src/main/java/live/smoothing/sensordata/adapter/Url.java
@@ -1,2 +1,18 @@
-package live.smoothing.sensordata.adapter;public class Url {
+package live.smoothing.sensordata.adapter;
+
+import lombok.Getter;
+
+@Getter
+public class Url {
+    private String url = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?" +
+            "year=%d&" +
+            "month=%s&" +
+            "bizCd=%s&" +
+            "apiKey=kG5U7b415709zni9P0588V4R2KHh65ITvO95GNM9&returnType=json";
+
+    public String getUrl(int year, String month, String bizCd) {
+        return String.format(url, year, month, bizCd);
+    }
+
+
 }
