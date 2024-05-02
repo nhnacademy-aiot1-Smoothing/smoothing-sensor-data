da69a69534ecb919d162dad77451358273e9ec3b
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-02 19:51:38 +0900

    [Feat]: 인터페이스 구현

--- src/main/java/live/smoothing/sensordata/adapter/EnergyUsageApiAdapter.java
+++ src/main/java/live/smoothing/sensordata/adapter/EnergyUsageApiAdapter.java
@@ -1,5 +1,8 @@
 package live.smoothing.sensordata.adapter;
 
+import live.smoothing.sensordata.dto.usage.EnergyUsageResponse;
+
 public interface EnergyUsageApiAdapter {
 
+    EnergyUsageResponse fetchEnergyUsage(int year, String month, String bizCd);
 }
