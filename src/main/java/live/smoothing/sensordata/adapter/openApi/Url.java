8d2e93463a833761ad1c72eb790a81df68fdd956
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-02 20:46:14 +0900

    [Feat]: Url 빈 구현

--- src/main/java/live/smoothing/sensordata/adapter/openApi/Url.java
+++ src/main/java/live/smoothing/sensordata/adapter/openApi/Url.java
@@ -4,14 +4,16 @@ import lombok.Getter;
 
 @Getter
 public class Url {
-    private String url = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?" +
-            "year=%d&" +
-            "month=%s&" +
-            "bizCd=%s&" +
-            "apiKey=kG5U7b415709zni9P0588V4R2KHh65ITvO95GNM9&returnType=json";
 
-    public String getUrl(int year, String month, String bizCd) {
-        return String.format(url, year, month, bizCd);
+    private final String baseUrl;
+    private final String apiKey;
+
+    public Url(String baseUrl, String apiKey) {
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
     }
 
+    public String getUrl(int year, String month, String bizCd) {
+        return String.format(this.baseUrl, year, month, bizCd) + "&apiKey=" + this.apiKey + "&returnType=json";
+    }
 }
