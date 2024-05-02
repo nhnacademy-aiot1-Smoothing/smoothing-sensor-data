b23b73f4125d9f2e2b1472bc00237526b2fcf475
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-02 20:48:20 +0900

    [Feat]: KEPCOEnergyUsageApiAdapter 클래스 추가
    수신된 응담 구분을 파싱하여 OpenAPI에서 에너지 사용량 데이터를 가져올 수 있음

---
+++ src/main/java/live/smoothing/sensordata/adapter/openApi/KEPCOEnergyUsageApiAdapter.java
@@ -0,0 +1,44 @@
+package live.smoothing.sensordata.adapter.openApi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import live.smoothing.common.exception.CommonException;
+import live.smoothing.sensordata.adapter.EnergyUsageApiAdapter;
+import live.smoothing.sensordata.dto.usage.EnergyUsageResponse;
+import live.smoothing.sensordata.util.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class KEPCOEnergyUsageApiAdapter implements EnergyUsageApiAdapter {
+
+    private final Url urlProvider;
+
+    @Autowired
+    public KEPCOEnergyUsageApiAdapter(Url urlProvider) {
+        this.urlProvider = urlProvider;
+    }
+
+    @Override
+    public EnergyUsageResponse fetchEnergyUsage(int year, String month, String bizCd) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = urlProvider.getUrl(year, month, bizCd);
+        ResponseEntity<String> resp = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        return parseResponse(resp.getBody());
+    }
+
+    private EnergyUsageResponse parseResponse(String json) throws CommonException {
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            String fixedJson = JsonUtil.fixJsonArray(json);
+            return mapper.readValue(fixedJson, EnergyUsageResponse.class);
+        } catch(Exception e) {
+            throw  new CommonException(HttpStatus.BAD_REQUEST, "KEPCO API 호출 실패: " + e.getMessage());
+        }
+    }
+}
