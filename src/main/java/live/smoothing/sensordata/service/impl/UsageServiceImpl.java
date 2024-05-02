085f69343c9eebdfed2e945729f303d04a23a6ca
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-03 07:54:31 +0900

    [Feat]: 평균 사용량 서비스

---
+++ src/main/java/live/smoothing/sensordata/service/impl/UsageServiceImpl.java
@@ -0,0 +1,24 @@
+package live.smoothing.sensordata.service.impl;
+
+import live.smoothing.sensordata.adapter.openApi.KEPCOEnergyUsageApiAdapter;
+import live.smoothing.sensordata.dto.usage.EnergyUsageResponse;
+import live.smoothing.sensordata.service.UsageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsageServiceImpl implements UsageService {
+
+    private final KEPCOEnergyUsageApiAdapter apiAdapter;
+
+    @Autowired
+    public UsageServiceImpl(KEPCOEnergyUsageApiAdapter apiAdapter) {
+        this.apiAdapter = apiAdapter;
+    }
+
+    @Override
+    public EnergyUsageResponse getEnergyUsage(int year, String month, String bizCd) {
+        return apiAdapter.fetchEnergyUsage(year, month, bizCd);
+    }
+}
+`
