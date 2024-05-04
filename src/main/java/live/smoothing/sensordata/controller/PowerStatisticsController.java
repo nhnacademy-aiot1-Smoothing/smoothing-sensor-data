5b23ddb4f51f4de4b6c898043315cd49b52e791d
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-04 23:27:33 +0900

    [Feat]: 전력조회 컨트롤러

--- src/main/java/live/smoothing/sensordata/controller/PowerStatisticsController.java
+++ src/main/java/live/smoothing/sensordata/controller/PowerStatisticsController.java
@@ -1,2 +1,19 @@
-package live.smoothing.sensordata.controller;public class PowerStatisticsController {
+package live.smoothing.sensordata.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PowerStatisticsController {
+
+    @GetMapping("/api/sensor/electrical/range")
+    public String getElectricalRange(@RequestParam String type,
+                                     @RequestParam Integer unit) {
+
+        return null;
+    }
+
 }
