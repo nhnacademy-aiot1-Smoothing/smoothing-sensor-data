5371182d0d74b06c1aaf5b566be7d8e2e6161515
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-07 20:58:49 +0900

    [Feat]: 전력 통계 DTO

--- src/main/java/live/smoothing/sensordata/dto/statistics/KwhTimeSeriesResponse.java
+++ src/main/java/live/smoothing/sensordata/dto/statistics/KwhTimeSeriesResponse.java
@@ -2,15 +2,16 @@ package live.smoothing.sensordata.dto.statistics;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
-public class PowerUsageStatisticsResponse {
-    private List<Double> weeklyUsage;
-    private List<Double> monthlyUsage;
-    private List<Double> yearlyUsage;
+public class KwhTimeSeriesResponse {
+    private String label;
+    private List<Double> values;
 }
