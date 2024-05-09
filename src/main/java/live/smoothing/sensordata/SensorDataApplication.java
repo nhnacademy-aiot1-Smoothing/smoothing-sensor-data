559173522ababf94b24afa393b36d51391c2af8e
nakta-y <nakta.y.biz@gmail.com>
2024-05-09 13:29:14 +0900

    [Fix] : 전력 조회 시간대 수정

--- src/main/java/live/smoothing/sensordata/SensorDataApplication.java
+++ src/main/java/live/smoothing/sensordata/SensorDataApplication.java
@@ -3,11 +3,8 @@ package live.smoothing.sensordata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-import javax.swing.*;
 
 @SpringBootApplication
 @EnableDiscoveryClient
