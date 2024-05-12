f2e42baf3e41287e98269218719cb1fa44afba2d
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-12 14:37:21 +0900

    [Fix]: 기능에 맞게 DTO 수정

--- src/main/java/live/smoothing/sensordata/dto/usage/EnergyUsageResponse.java
+++ src/main/java/live/smoothing/sensordata/dto/usage/EnergyUsageResponse.java
@@ -1,15 +1,12 @@
 package live.smoothing.sensordata.dto.usage;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
 public class EnergyUsageResponse {
 
-    @JsonProperty("data")
-    List<EnergyUsage> data;
+    private EnergyUsage wholeCountry;
+    private EnergyUsage kimCity;
 }
