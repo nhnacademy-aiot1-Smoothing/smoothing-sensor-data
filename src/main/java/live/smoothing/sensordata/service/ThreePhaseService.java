37a36fbd537018c7c108d77ce050b3231ee7b2c8
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-05-12 18:58:15 +0900

    [Feat]: 3상 서비스 구현

---
+++ src/main/java/live/smoothing/sensordata/service/ThreePhaseService.java
@@ -0,0 +1,7 @@
+package live.smoothing.sensordata.service;
+
+import live.smoothing.sensordata.dto.phase.PhaseResponse;
+
+public interface ThreePhaseService {
+    PhaseResponse getThreePhase();
+}
